0afeaf9b0f2f4af9bbe42ca870293d1c5b09d5e9
Erik "Bardez" Frantz <bardez@bardez.net>
2020-10-05 17:31:10 -0500

    Fiddling with the graphic; tidying up lua

--- objects/liquid/sensor/bardez_liq_comp_sensor.lua
+++ objects/liquid/sensor/bardez_liq_comp_sensor.lua
@@ -1,6 +1,5 @@
 
 function init()
-
 	if storage == nil then
 		storage = {}
 	end
@@ -16,20 +15,28 @@ function update(dt)
 	--	  float amount
 	local liquidLevel = world.liquidAt(storage.position)
 
-	local amount = math.floor(liquidLevel and liquidLevel[2] or 0)
-	local display = "00" .. tostring(amount)
+	if liquidLevel and liquidLevel[2] then
+		local amount = math.floor(liquidLevel and liquidLevel[2] or 0)
+		local display = "00" .. tostring(amount)
 
-	if not tonumber(liquidLevel[2]) then
+		if amount <= 999 then
+			animator.setAnimationState("one", string.sub(display,-1))
+			animator.setAnimationState("ten", string.sub(display,-2,-2))
+			animator.setAnimationState("hundred", string.sub(display,-3,-3))
+
+			--output true, that it is safe to continue
+			object.setOutputNodeLevel(0, true)
+		else
+			animator.setAnimationState("one", "excess")
+			animator.setAnimationState("ten", "excess")
+			animator.setAnimationState("hundred", "excess")
+
+			--output false, that it is NOT safe to continue
+			object.setOutputNodeLevel(0, false)
+		end
+	else
 		animator.setAnimationState("one", "invalid")
 		animator.setAnimationState("ten", "invalid")
 		animator.setAnimationState("hundred", "invalid")
-	elseif tonumber(liquidLevel[2]) <= 999 then
-		animator.setAnimationState("one", string.sub(display,-1))
-		animator.setAnimationState("ten", string.sub(display,-2,-2))
-		animator.setAnimationState("hundred", string.sub(display,-3,-3))
-	else
-		animator.setAnimationState("one", "excess")
-		animator.setAnimationState("ten", "excess")
-		animator.setAnimationState("hundred", "excess")
 	end
 end
